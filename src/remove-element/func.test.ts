a952de254b96fc5f830b083888c84da584d27277
Bill Fienberg <billfienberg@gmail.com>
2021-09-07 23:01:19 -0500

    Add WIP solution that passes test 1

--- src/remove-element/func.test.ts
+++ src/remove-element/func.test.ts
@@ -1,5 +1,39 @@
 function removeElement(nums: number[], val: number): number {
-  return 81
+  let result = 0
+  let indexToBeUpdated = 0
+  const rows = []
+  const lastIndex = nums.length - 1
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.length > 100) {
+      throw new Error("You created an infinte loop")
+    }
+    const islastIndex = i === lastIndex
+    const before = [...nums].join(",")
+    const currentIndex = i
+    const nextIndex = i + 1
+    const [current, next] = [nums[i], nums[nextIndex]]
+    // When are we able to splice?
+
+    const shouldSplice = current === val && !islastIndex
+
+    // last element
+    if (islastIndex) {
+    } else {
+      // all other elements
+      nums.splice(indexToBeUpdated, 2, next, 81)
+      indexToBeUpdated++
+      if (nextIndex !== lastIndex) {
+        result++
+      }
+    }
+
+    const after = [...nums].join(",")
+    const row = { islastIndex, current, next, shouldSplice, indexToBeUpdated, numsBefore: before, numsAfter: after }
+    rows.push(row)
+  }
+  console.table(rows)
+  console.log({ result })
+  return result
 }
 
 describe("removeElement", () => {
