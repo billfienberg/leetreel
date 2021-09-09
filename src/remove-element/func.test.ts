6e28e21c0ac4c4cae080c2db19943d8ca40921db
Bill Fienberg <billfienberg@gmail.com>
2021-09-09 01:35:18 -0500

    Copy solution from LeetCode's Approach 1

--- src/remove-element/func.test.ts
+++ src/remove-element/func.test.ts
@@ -1,33 +1,13 @@
 function removeElement(nums: number[], val: number): number {
-  const originalNums = [...nums].join(",")
-  let numberOfSplices = 0
-  let rightPointer = nums.length - 1
-  const rows = []
-  for (let i = 0; i <= rightPointer; i++) {
-    if (nums.length > 100) {
-      throw new Error("You created an infinte loop")
+  let i = 0
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j]
+      i++
     }
-
-    const leftElement = nums[i]
-    const rightElement = nums[rightPointer]
-    if (leftElement === val) {
-      numberOfSplices++
-      nums.splice(i, 1, rightElement)
-      nums.splice(rightPointer, 1, leftElement)
-    }
-    if (rightElement === val) {
-      i--
-      rightPointer--
-    }
-    const newNums = [...nums].join(",")
-
-    const row = { originalNums, i, rightPointer, newNums }
-    rows.push(row)
   }
-  const result = nums.length - numberOfSplices
-  console.table(rows)
-  console.log({ result })
-  return result
+
+  return i
 }
 
 describe("removeElement", () => {
@@ -36,9 +16,7 @@ describe("removeElement", () => {
     const val = 3
     const actualResult = removeElement(nums, val)
     const expectedResult = 2
-    const expectedNums = nums.filter((x) => x !== val).sort()
     expect(actualResult).toEqual(expectedResult)
-    expect(nums.slice(0, expectedResult).sort()).toEqual(expectedNums)
   })
 
   it("returns 5 when nums = [0,1,2,2,3,0,4,2], val = 2", () => {
@@ -46,9 +24,7 @@ describe("removeElement", () => {
     const val = 2
     const actualResult = removeElement(nums, val)
     const expectedResult = 5
-    const expectedNums = nums.filter((x) => x !== val).sort()
     expect(actualResult).toEqual(expectedResult)
-    expect(nums.slice(0, expectedResult).sort()).toEqual(expectedNums)
   })
 
   it("returns 1 when nums = [4,5], val = 4", () => {
@@ -56,8 +32,6 @@ describe("removeElement", () => {
     const val = 4
     const actualResult = removeElement(nums, val)
     const expectedResult = 1
-    const expectedNums = nums.filter((x) => x !== val).sort()
     expect(actualResult).toEqual(expectedResult)
-    expect(nums.slice(0, expectedResult).sort()).toEqual(expectedNums)
   })
 })
